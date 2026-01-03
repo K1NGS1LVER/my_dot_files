3869496dc26f6ff5036ca5e31b6fd7055fabf19f
Daniel paul <booyahkaasah@gmail.com>
2026-01-03 16:07:25 +0530

    feat: add initial bash and fish configurations

---
+++ bash/.bashrc
@@ -0,0 +1,38 @@
+# ~/.bashrc
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+# --- PATH ---
+export PATH="$HOME/.local/bin:$HOME/.cargo/bin:/opt/homebrew/bin:$PATH"
+
+# --- TOOLS ---
+# Starship (Prompt)
+# eval "$(starship init bash)"
+
+# Zoxide (Smart CD)
+eval "$(zoxide init bash)"
+
+# Atuin (History)
+[[ -f /opt/homebrew/bin/atuin ]] && eval "$(atuin init bash)"
+
+# --- ALIASES ---
+alias ls='eza --icons'
+alias ll='eza -lah --icons --git'
+alias la='eza -A --icons'
+alias c='clear'
+alias home='cd ~'
+
+# Editor
+alias vim='nvim'
+alias vi='nvim'
+alias v='nvim'
+
+# Git
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+
+# Todo Tool (Go)
+alias todo="todo-go list '(today | overdue | #Inbox | recurring)' | fzf --delimiter=$'\t' --with-nth=2 --header 'Inbox, Today & Recurring: Select to complete (ESC cancel)' --height 40% --reverse | awk '{print \$1}' | xargs todo-go close"
---
+++ fish/config.fish
@@ -0,0 +1,42 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # --- ALIASES ---
+    alias ls='eza --icons'
+    alias ll='eza -lah --icons --git'
+    alias la='eza -A --icons'
+    alias c='clear'
+    alias home='cd ~'
+    
+    # Git
+    alias gs='git status'
+    alias ga='git add'
+    alias gc='git commit'
+    alias gp='git push'
+
+    # Editor
+    alias vim='nvim'
+    alias vi='nvim'
+    alias v='nvim'
+
+    # --- TOOLS ---
+    # Initialize Zoxide (smarter cd)
+    zoxide init fish | source
+
+    # Initialize Atuin (Magic History)
+    if test -f /opt/homebrew/bin/atuin
+        atuin init fish | source
+    end
+
+    # Starship Prompt (Highly recommended for cross-shell unity)
+    # starship init fish | source
+
+    # --- TODO TOOL ---
+    # Using your new Go binary
+    alias todo="todo-go list '(today | overdue | #Inbox | recurring)' | fzf --delimiter='\t' --with-nth=2 --header 'Inbox, Today & Recurring: Select to complete (ESC cancel)' --height 40% --reverse | awk '{print \$1}' | xargs todo-go close"
+
+    # Fix PATH to include local bin and cargo
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.cargo/bin
+    fish_add_path /opt/homebrew/bin
+end
