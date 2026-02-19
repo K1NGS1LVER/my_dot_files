c831bd158a510710095ac5db22484e674a92694e
Daniel paul <booyahkaasah@gmail.com>
2026-02-20 00:35:15 +0530

    added mini.nvim based config

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,4 +1,16 @@
 require "nvchad.autocmds"
 
-local vim = vim
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("InsertEnter", {
+  callback = function()
+    vim.opt.relativenumber = false
+  end,
+})
+
+autocmd("InsertLeave", {
+  callback = function()
+    vim.opt.relativenumber = true
+  end,
+})
 
