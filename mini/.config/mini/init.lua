f32d763d7e4be85838ea6f19ac87da01d2220ba1
Daniel paul <booyahkaasah@gmail.com>
2026-02-21 10:57:49 +0530

    chore: hardcoded yazi fix

--- mini/.config/mini/init.lua
+++ mini/.config/mini/init.lua
@@ -5,6 +5,20 @@ vim.g.maplocalleader = " "
 -- Global transparency state
 vim.g.transparency_enabled = true
 
+-- Basic Options
+local o = vim.o
+o.number = true
+o.relativenumber = true
+o.expandtab = true
+o.shiftwidth = 2
+o.tabstop = 2
+o.smartindent = true
+o.ignorecase = true
+o.smartcase = true
+o.termguicolors = true
+o.splitbelow = true
+o.splitright = true
+
 -- Install lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -25,13 +39,11 @@ require("lazy").setup({
     "echasnovski/mini.nvim",
     version = "*",
     config = function()
-      -- Mini.basics: Sets up vim.opt and basic mappings
       require("mini.basics").setup({
         options = { basic = true, extra_ui = true, win_borders = "default" },
         mappings = { basic = true, option_toggle_prefix = [[<leader>t]], windows = true, move_with_alt = true },
         autocommands = { basic = true, relnum_in_visual_mode = true },
       })
-
       require("mini.ai").setup()
       require("mini.align").setup()
       require("mini.bracketed").setup()
@@ -39,29 +51,16 @@ require("lazy").setup({
       require("mini.comment").setup()
       require("mini.completion").setup({
         lsp_completion = { source_func = "completefunc", auto_setup = false },
-        window = {
-          info = { height = 25, width = 80, border = 'rounded' },
-          signature = { height = 25, width = 80, border = 'rounded' },
-        },
       })
       require("mini.cursorword").setup()
-      require("mini.diff").setup({
-        view = { style = "sign", signs = { add = "│", change = "│", delete = "_" } },
-      })
+      require("mini.diff").setup({ view = { style = "sign" } })
       require("mini.extra").setup()
       require("mini.files").setup({ windows = { preview = true, width_preview = 80 } })
       require("mini.git").setup()
       require("mini.icons").setup()
       if _G.MiniIcons then MiniIcons.mock_nvim_web_devicons() end
-
-      require("mini.indentscope").setup({
-        symbol = "│",
-        draw = { animation = require("mini.indentscope").gen_animation.none() },
-      })
-      require("mini.jump2d").setup({
-        mappings = { start_jumping = 's' },
-        view = { dim = true },
-      })
+      require("mini.indentscope").setup({ symbol = "│", draw = { animation = require("mini.indentscope").gen_animation.none() } })
+      require("mini.jump2d").setup({ mappings = { start_jumping = 's' } })
       require("mini.move").setup({
         mappings = {
           left = '<M-h>', right = '<M-l>', down = '<M-j>', up = '<M-k>',
@@ -71,7 +70,7 @@ require("lazy").setup({
       require("mini.notify").setup()
       vim.notify = require("mini.notify").make_notify()
       require("mini.pairs").setup()
-      require("mini.pick").setup({ mappings = { move_down = '<C-n>', move_up = '<C-p>' } })
+      require("mini.pick").setup()
       require("mini.sessions").setup({ autoread = false, autowrite = true })
       require("mini.starter").setup()
       require("mini.statusline").setup()
@@ -79,22 +78,33 @@ require("lazy").setup({
       require("mini.tabline").setup()
       require("mini.trailspace").setup()
       require("mini.visits").setup()
+
+      -- Custom mappings for MiniFiles
+      vim.api.nvim_create_autocmd('User', {
+        pattern = 'MiniFilesBufferCreate',
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set('n', '<CR>', function() MiniFiles.go_in({ close_on_file = true }) end, { buffer = buf_id })
+        end,
+      })
     end,
   },
 
-  -- Treesitter for highlighting
+  -- Treesitter
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    cmd = { "TSUpdateSync", "TSUpdate" },
     config = function()
       local status, ts = pcall(require, "nvim-treesitter.configs")
       if status then
         ts.setup({
-          ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "c", "python", "javascript", "typescript", "go", "rust" },
+          ensure_installed = { 
+            "lua", "vim", "vimdoc", "markdown", "markdown_inline", 
+            "python", "javascript", "typescript", "go", "rust", 
+            "html", "css", "c", "cpp" 
+          },
           highlight = { enable = true },
-          indent = { enable = true },
         })
       end
     end,
@@ -110,112 +120,81 @@ require("lazy").setup({
     config = function()
       require("mason").setup()
       local mlsp = require("mason-lspconfig")
-      mlsp.setup({ ensure_installed = { "lua_ls" } })
+      
+      local servers = {
+        "html", "cssls", "pyright", "ruff", "gopls", "rust_analyzer",
+        "lua_ls", "kotlin_language_server", "jdtls", "bashls", "jsonls",
+        "ts_ls", "eslint", "clangd"
+      }
+      
+      mlsp.setup({ ensure_installed = servers })
 
-      local lspconfig = require("lspconfig")
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-      local on_attach = function(client, bufnr)
+      local on_attach = function(_, bufnr)
         local opts = { buffer = bufnr, noremap = true, silent = true }
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
         vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
         vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>lf', function() vim.lsp.buf.format { async = true } end, opts)
       end
 
-      if mlsp.setup_handlers then
-        mlsp.setup_handlers({
-          function(server_name)
-            lspconfig[server_name].setup({ on_attach = on_attach, capabilities = capabilities })
-          end,
-        })
-      else
-        lspconfig.lua_ls.setup({ on_attach = on_attach, capabilities = capabilities })
+      -- The stable Neovim 0.11 API pattern
+      for _, name in ipairs(servers) do
+        local opts = { on_attach = on_attach }
+        
+        if name == "pyright" then
+          opts.settings = { python = { analysis = { typeCheckingMode = "off" } } }
+        end
+
+        local status, config = pcall(function() return vim.lsp.config[name] end)
+        if status and config then
+          config.setup(opts)
+        end
       end
     end,
   },
 
-  -- Theme: Catppuccin with Transparency support
+  -- Theme
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
     config = function()
-      require("catppuccin").setup({ 
-        flavour = "mocha",
-        transparent_background = vim.g.transparency_enabled,
-        integrations = {
-          mini = { enabled = true },
-          native_lsp = { enabled = true },
-          treesitter = true,
-        }
-      })
+      require("catppuccin").setup({ flavour = "mocha", transparent_background = vim.g.transparency_enabled })
       vim.cmd.colorscheme "catppuccin"
     end,
   }
 })
 
--- ========================================================================== --
---                            CUSTOM MAPPINGS                                 --
--- ========================================================================== --
-
+-- Mappings
 local map = vim.keymap.set
 
--- Transparency Toggle (from your main config)
+-- Terminal
+map("n", "<leader>h", "<cmd>split | term<cr>i", { desc = "Horizontal Terminal" })
+map("n", "<leader>v", "<cmd>vsplit | term<cr>i", { desc = "Vertical Terminal" })
+map("t", "jk", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+
+-- Transparency
 map("n", "<leader>tt", function()
   vim.g.transparency_enabled = not vim.g.transparency_enabled
   require("catppuccin").setup({ transparent_background = vim.g.transparency_enabled })
   vim.cmd("colorscheme catppuccin")
-  print("Transparency: " .. tostring(vim.g.transparency_enabled))
 end, { desc = "Toggle Transparency" })
 
 -- Mini.Files
 map("n", "<leader>e", function() 
-  if _G.MiniFiles then
-    if not MiniFiles.close() then MiniFiles.open() end 
+  if _G.MiniFiles then 
+    if not MiniFiles.close() then 
+      local path = vim.api.nvim_buf_get_name(0)
+      if path == "" or vim.fn.filereadable(path) == 0 then path = vim.fn.getcwd() end
+      MiniFiles.open(path) 
+    end 
   end
 end, { desc = "Toggle Mini Files" })
 
 -- Mini.Pick
 map("n", "<leader>ff", "<cmd>Pick files<cr>", { desc = "Find Files" })
 map("n", "<leader>fw", "<cmd>Pick grep_live<cr>", { desc = "Live Grep" })
-map("n", "<leader>fb", "<cmd>Pick buffers<cr>", { desc = "Find Buffers" })
-map("n", "<leader>fh", "<cmd>Pick help<cr>", { desc = "Help Tags" })
-
--- General
 map("n", "<C-s>", "<cmd>w<cr>", { desc = "Save" })
-map("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
 map("n", "<leader>qq", "<cmd>q<cr>", { desc = "Quit" })
-map("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear Search Highlights" })
-
--- Better indenting
-map("v", "<", "<gv", { desc = "Indent Left" })
-map("v", ">", ">gv", { desc = "Indent Right" })
-
--- Window Resizing
-map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase window height" })
-map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase window width" })
-
--- PDF Viewer
-vim.api.nvim_create_user_command("Pdf", function(opts)
-  local filepath = opts.args == "" and vim.fn.expand("%:p") or opts.args
-  vim.fn.jobstart({ "/Applications/sioyek.app/Contents/MacOS/sioyek", "--new-window", filepath }, { detach = true })
-end, { nargs = "?", complete = "file" })
-
--- Basic Options
-local o = vim.o
-o.number = true
-o.relativenumber = true
-o.expandtab = true
-o.shiftwidth = 2
-o.tabstop = 2
-o.smartindent = true
-o.ignorecase = true
-o.smartcase = true
-o.splitbelow = true
-o.splitright = true
-o.termguicolors = true
+map("n", "<Esc>", "<cmd>nohlsearch<cr>")
